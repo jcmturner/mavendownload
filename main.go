84d70f30fb39140f4296eb219bfe0c9eb082cbe5
Jonathan Turner <jt@jtnet.co.uk>
2018-05-24 21:33:18 +0100

    error check

--- main.go
+++ main.go
@@ -14,13 +14,25 @@ func main() {
 	grpID := flag.String("groupid", "", "GroupID of artifact")
 	artifactID := flag.String("artifactid", "", "ArtifactID of artifact")
 	ext := flag.String("ext", "", "Override file extension")
-	out := flag.String("out", "", "Output path")
+	out := flag.String("out", "./", "Output path")
 	flag.Parse()
 
+	if !strings.HasPrefix(*repo, "http://") && !strings.HasPrefix(*repo, "https://") {
+		fmt.Fprintln(os.Stderr, "Repo URL is invalid. Must start http:// or https://")
+		os.Exit(1)
+	}
+	if *grpID == "" {
+		fmt.Fprintln(os.Stderr, "GroupID is not defined")
+		os.Exit(1)
+	}
+	if *artifactID == "" {
+		fmt.Fprintln(os.Stderr, "ArtifactID is not defined")
+		os.Exit(1)
+	}
 	n, fname, err := download.Latest(*repo, *grpID, *artifactID, *ext, *out)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error downloading: %v", err)
 		os.Exit(1)
 	}
-	fmt.Printf("Downloaded %d bytes to %s/%s", n, strings.TrimRight(*out, "/"), fname)
+	fmt.Printf("Output: %s/%s\nSize: %d", strings.TrimRight(*out, "/"), fname, n)
 }
